c2b49b4e9d5a509b1daf3eaab1664f93fc01bcd1
Akash patil <96292097+ranthamGOD@users.noreply.github.com>
2024-03-12 13:17:23 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accoudtId = 2131232;
+let accountEmail = "ap412132@gmail.com"
+var accountPassword = "uwne2";
+accountCity = "Noida";
+
+// accoudtId = 21323; accoundId is a constant we cannot change it 
+console.table([accoudtId,accountEmail,accountPassword,accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"  ; // treat all js code as newer versions
+
+// alert (3 + 3) ; // we are using node.js not the browser
+
+console.log( 3   // code redability should be high
+    + 3 );
+    // data types ( primitive- data types )
+// numbers => 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// symbol  => unique
+
+console.log(typeof "akash");
+console.log(typeof 1233);
