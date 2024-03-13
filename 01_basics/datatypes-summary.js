7dc3b3adf1bb71e68a6681fa6918e19c9a8061d2
Akash patil <96292097+ranthamGOD@users.noreply.github.com>
2024-03-13 18:50:02 +0000

    datatypes-summary code

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,17 @@
+// call by value (Primitive Data types)
+
+// 
+
+
+
+
+
+
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId);
+
+// Reference type call by refrence OR ( Non-primitive data types)
+
+// Array , Object , functions
